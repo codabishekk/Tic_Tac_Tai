ef1003c8373a2f683524339a9d83c83914fe63c5
codabishekk <abishekkc923@gmail.com>
2026-02-07 19:14:27 +0530

    fix: Add custom Vite plugin to ensure correct MIME type for JavaScript and JSX files.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: 'fix-mime-type',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url?.endsWith('.js') || req.url?.endsWith('.jsx')) {
+            res.setHeader('Content-Type', 'text/javascript');
+          }
+          next();
+        });
+      }
+    }
+  ],
 })
